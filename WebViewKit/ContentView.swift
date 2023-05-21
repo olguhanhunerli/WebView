8740d3fee4c6895cac5f1bc456941e963ef68378
olguhanhunerli <olguhanhunerli@gmail.com>
2023-05-21 19:46:48 +0300

    Web View Hazırlandı

---
+++ WebViewKit/ContentView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  WebViewKit
+//
+//  Created by Olguhan Hünerli on 21.05.2023.
+//
+
+import SwiftUI
+import WebKit
+
+struct ContentView: View {
+    @State private var urlString = ""
+    var body: some View {
+    WebView(urlString: urlString)
+        
+    }
+}
+struct WebView: UIViewRepresentable {
+    
+    let urlString: String
+   
+    func makeUIView(context: Context) -> WKWebView {
+        guard let url = URL(string: urlString) else {
+            return WKWebView()
+        }
+        let request = URLRequest(url:url)
+        let webView = WKWebView()
+        webView.load(request)
+        return webView
+    }
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        guard let url = URL(string: urlString) else {
+            return
+        }
+        let request = URLRequest(url: url)
+        uiView.load(request)
+    }
+    
+}
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WebViewKit/WebViewKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebViewKitApp.swift
+//  WebViewKit
+//
+//  Created by Olguhan Hünerli on 21.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct WebViewKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
